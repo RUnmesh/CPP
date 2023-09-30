f833f77848c330be7313b6ad5db5e4f01d0871d6
runmesh <unmeshr.18@gmail.com>
2023-09-30 12:06:32 +0530

    Modify template

--- template/template.cc
+++ template/template.cc
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
+#define f(i, start, end, interval) for(ll i = start; i < end; i = i + interval)
+#define cf(i, start, end, interval) for(ll i = start; i <= end; i = i + interval)
+
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? a : gcd(b, a%b);
+}
+
 
 int main()
 {
-
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt","r",stdin);
 	freopen("output.txt","w",stdout);
 	#endif
 
-    
 
 	return 0;
 }
